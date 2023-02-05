9b1b5c60d657f861e3a38ec6a439b25eea40059f
李槟 <1961774471@qq.com>
2023-02-05 23:35:16 +0800

    1.修改写死的数据

--- src/pages/list/base-node-time-list/index.tsx
+++ src/pages/list/base-node-time-list/index.tsx
@@ -142,92 +142,90 @@ const TableList: React.FC = () => {
             data: {
               records: [
                 {
-                  usoOrderNo: {
-                    serviceType: null,
-                    no: '113011',
-                    transType: null,
-                    B8F385C4E8DD404FB955564A0B48EF1A: {
-                      '933565A0E2D84F4B987B9D48B2C23A02': {
-                        smallNodeTime: ['60分钟', '60分钟'],
-                        aging: '正常 120分钟',
-                        smallNodeName: ['初始', '审核'],
-                      },
-                      FDB8A6174EE24DF28A06A332CB3AF290: {
-                        smallNodeTime: ['60分钟', '60分钟'],
-                        aging: '正常 120分钟',
-                        smallNodeName: ['初始', '审核'],
-                      },
+                  serviceType: null,
+                  no: '113011',
+                  transType: null,
+                  B8F385C4E8DD404FB955564A0B48EF1A: {
+                    '933565A0E2D84F4B987B9D48B2C23A02': {
+                      smallNodeTime: ['60分钟', '60分钟'],
+                      aging: '正常 120分钟',
+                      smallNodeName: ['初始', '审核'],
                     },
-                    '5833520DE86E4C93A969A1E0C035000E': {
-                      '337CC46F72BA4D7F9D671D09811255E4': {
-                        smallNodeTime: ['60分钟', '60分钟'],
-                        aging: '正常 120分钟',
-                        smallNodeName: ['初始', '审核'],
-                      },
-                      '2CB097DC3D9D49348627EE67CD084FCD': {
-                        smallNodeTime: ['60分钟', '60分钟'],
-                        aging: '正常 120分钟',
-                        smallNodeName: ['初始', '审核'],
-                      },
-                      '28F28765B1504FC38046F61E2B22E3AF': {
-                        smallNodeTime: ['60分钟', '60分钟'],
-                        aging: '正常 120分钟',
-                        smallNodeName: ['初始', '审核'],
-                      },
-                      '62DDD6D6C3FB44B9B934F177D862C122': {
-                        smallNodeTime: ['60分钟', '60分钟'],
-                        aging: '正常 120分钟',
-                        smallNodeName: ['初始', '审核'],
-                      },
-                      '069EB075765349F6923524C192A98E94': {
-                        smallNodeTime: ['60分钟', '60分钟'],
-                        aging: '正常 120分钟',
-                        smallNodeName: ['初始', '审核'],
-                      },
-                      '39BAB84FFEBF411789EC1766F3C6F369': {
-                        smallNodeTime: ['60分钟', '60分钟'],
-                        aging: '正常 120分钟',
-                        smallNodeName: ['初始', '审核'],
-                      },
-                      D0F7DF97761C49899487D315FA2AE1DA: {
-                        smallNodeTime: ['60分钟', '60分钟'],
-                        aging: '正常 120分钟',
-                        smallNodeName: ['初始', '审核'],
-                      },
-                      '592326EE61BB45C08787319422ED7082': {
-                        smallNodeTime: ['60分钟', '60分钟'],
-                        aging: '正常 120分钟',
-                        smallNodeName: ['初始', '审核'],
-                      },
-                      '1E7765AB555946A0915E559587D85967': {
-                        smallNodeTime: ['60分钟', '60分钟'],
-                        aging: '正常 120分钟',
-                        smallNodeName: ['初始', '审核'],
-                      },
-                      '7D418FDA8CAC49AEA6DAB4F1DFC3D824': {
-                        smallNodeTime: ['60分钟', '60分钟'],
-                        aging: '正常 120分钟',
-                        smallNodeName: ['初始', '审核'],
-                      },
-                      '81938E76D2874C8881433422BA3FDEF0': {
-                        smallNodeTime: ['60分钟', '60分钟'],
-                        aging: '正常 120分钟',
-                        smallNodeName: ['初始', '审核'],
-                      },
-                      E6B0BFA5CE63443EBFE1C832F7D5B804: {
-                        smallNodeTime: ['60分钟', '60分钟'],
-                        aging: '正常 120分钟',
-                        smallNodeName: ['初始', '审核'],
-                      },
-                      CC7C6AB9312C4A0199B7A0DD0E443E26: {
-                        smallNodeTime: ['60分钟', '60分钟'],
-                        aging: '正常 120分钟',
-                        smallNodeName: ['初始', '审核'],
-                      },
+                    FDB8A6174EE24DF28A06A332CB3AF290: {
+                      smallNodeTime: ['60分钟', '60分钟'],
+                      aging: '正常 120分钟',
+                      smallNodeName: ['初始', '审核'],
                     },
-                    ASD32WC4E8DD409FB923564A0B48EF1: {},
-                    '157A6975A4414D5585BB617EB2F095E1': {},
                   },
+                  '5833520DE86E4C93A969A1E0C035000E': {
+                    '337CC46F72BA4D7F9D671D09811255E4': {
+                      smallNodeTime: ['60分钟', '60分钟'],
+                      aging: '正常 120分钟',
+                      smallNodeName: ['初始', '审核'],
+                    },
+                    '2CB097DC3D9D49348627EE67CD084FCD': {
+                      smallNodeTime: ['60分钟', '60分钟'],
+                      aging: '正常 120分钟',
+                      smallNodeName: ['初始', '审核'],
+                    },
+                    '28F28765B1504FC38046F61E2B22E3AF': {
+                      smallNodeTime: ['60分钟', '60分钟'],
+                      aging: '正常 120分钟',
+                      smallNodeName: ['初始', '审核'],
+                    },
+                    '62DDD6D6C3FB44B9B934F177D862C122': {
+                      smallNodeTime: ['60分钟', '60分钟'],
+                      aging: '正常 120分钟',
+                      smallNodeName: ['初始', '审核'],
+                    },
+                    '069EB075765349F6923524C192A98E94': {
+                      smallNodeTime: ['60分钟', '60分钟'],
+                      aging: '正常 120分钟',
+                      smallNodeName: ['初始', '审核'],
+                    },
+                    '39BAB84FFEBF411789EC1766F3C6F369': {
+                      smallNodeTime: ['60分钟', '60分钟'],
+                      aging: '正常 120分钟',
+                      smallNodeName: ['初始', '审核'],
+                    },
+                    D0F7DF97761C49899487D315FA2AE1DA: {
+                      smallNodeTime: ['60分钟', '60分钟'],
+                      aging: '正常 120分钟',
+                      smallNodeName: ['初始', '审核'],
+                    },
+                    '592326EE61BB45C08787319422ED7082': {
+                      smallNodeTime: ['60分钟', '60分钟'],
+                      aging: '正常 120分钟',
+                      smallNodeName: ['初始', '审核'],
+                    },
+                    '1E7765AB555946A0915E559587D85967': {
+                      smallNodeTime: ['60分钟', '60分钟'],
+                      aging: '正常 120分钟',
+                      smallNodeName: ['初始', '审核'],
+                    },
+                    '7D418FDA8CAC49AEA6DAB4F1DFC3D824': {
+                      smallNodeTime: ['60分钟', '60分钟'],
+                      aging: '正常 120分钟',
+                      smallNodeName: ['初始', '审核'],
+                    },
+                    '81938E76D2874C8881433422BA3FDEF0': {
+                      smallNodeTime: ['60分钟', '60分钟'],
+                      aging: '正常 120分钟',
+                      smallNodeName: ['初始', '审核'],
+                    },
+                    E6B0BFA5CE63443EBFE1C832F7D5B804: {
+                      smallNodeTime: ['60分钟', '60分钟'],
+                      aging: '正常 120分钟',
+                      smallNodeName: ['初始', '审核'],
+                    },
+                    CC7C6AB9312C4A0199B7A0DD0E443E26: {
+                      smallNodeTime: ['60分钟', '60分钟'],
+                      aging: '正常 120分钟',
+                      smallNodeName: ['初始', '审核'],
+                    },
+                  },
+                  ASD32WC4E8DD409FB923564A0B48EF1: {},
+                  '157A6975A4414D5585BB617EB2F095E1': {},
                 },
               ],
               total: 185953,
@@ -235,107 +233,101 @@ const TableList: React.FC = () => {
               current: 1,
               orders: [],
               headerData: [
+                { title: 'uso单号', dataIndex: 'no' },
+                { title: 'scmc单号', dataIndex: 'scmcOrderNo' },
+                { title: 'mofc单号', dataIndex: 'mofcOrderNo' },
+                { title: 'lrp单号', dataIndex: 'lrpOrderNo' },
+                { title: '运输产品', dataIndex: 'transType' },
+                { title: '服务要求', dataIndex: 'serviceType' },
                 {
-                  title: 'uso单号',
-                  dataIndex: 'usoOrderNo',
+                  title: '业务',
+                  dataIndex: 'B8F385C4E8DD404FB955564A0B48EF1A',
                   children: [
-                    { title: 'no', dataIndex: 'usoOrderNo.no' },
-                    { title: 'scmc单号', dataIndex: 'usoOrderNo.scmcOrderNo' },
-                    { title: 'mofc单号', dataIndex: 'usoOrderNo.mofcOrderNo' },
-                    { title: 'lrp单号', dataIndex: 'usoOrderNo.lrpOrderNo' },
-                    { title: '运输产品', dataIndex: 'usoOrderNo.transType' },
-                    { title: '服务要求', dataIndex: 'usoOrderNo.serviceType' },
                     {
-                      title: '业务',
-                      dataIndex: 'usoOrderNo.B8F385C4E8DD404FB955564A0B48EF1A',
-                      children: [
-                        {
-                          title: '财务审核时效',
-                          dataIndex:
-                            'usoOrderNo.B8F385C4E8DD404FB955564A0B48EF1A.FDB8A6174EE24DF28A06A332CB3AF290',
-                        },
-                        {
-                          title: 'C端货源占用时效',
-                          dataIndex:
-                            'usoOrderNo.B8F385C4E8DD404FB955564A0B48EF1A.933565A0E2D84F4B987B9D48B2C23A02',
-                        },
-                      ],
+                      title: '财务审核时效',
+                      dataIndex:
+                        'B8F385C4E8DD404FB955564A0B48EF1A.FDB8A6174EE24DF28A06A332CB3AF290',
                     },
                     {
-                      title: '物流',
-                      dataIndex: 'usoOrderNo.5833520DE86E4C93A969A1E0C035000E',
-                      children: [
-                        {
-                          title: 'C端派单时效',
-                          dataIndex:
-                            'usoOrderNo.5833520DE86E4C93A969A1E0C035000E.069EB075765349F6923524C192A98E94',
-                        },
-                        {
-                          title: '确定物流方式时效',
-                          dataIndex:
-                            'usoOrderNo.5833520DE86E4C93A969A1E0C035000E.337CC46F72BA4D7F9D671D09811255E4',
-                        },
-                        {
-                          title: '归集时效',
-                          dataIndex:
-                            'usoOrderNo.5833520DE86E4C93A969A1E0C035000E.28F28765B1504FC38046F61E2B22E3AF',
-                        },
-                        {
-                          title: '配送时效',
-                          dataIndex:
-                            'usoOrderNo.5833520DE86E4C93A969A1E0C035000E.2CB097DC3D9D49348627EE67CD084FCD',
-                        },
-                        {
-                          title: '民生派单时效',
-                          dataIndex:
-                            'usoOrderNo.5833520DE86E4C93A969A1E0C035000E.7D418FDA8CAC49AEA6DAB4F1DFC3D824',
-                        },
-                        {
-                          title: 'C端出库时效',
-                          dataIndex:
-                            'usoOrderNo.5833520DE86E4C93A969A1E0C035000E.D0F7DF97761C49899487D315FA2AE1DA',
-                        },
-                        {
-                          title: '到车时效',
-                          dataIndex:
-                            'usoOrderNo.5833520DE86E4C93A969A1E0C035000E.592326EE61BB45C08787319422ED7082',
-                        },
-                        {
-                          title: '装车时效',
-                          dataIndex:
-                            'usoOrderNo.5833520DE86E4C93A969A1E0C035000E.39BAB84FFEBF411789EC1766F3C6F369',
-                        },
-                        {
-                          title: '在途时效',
-                          dataIndex:
-                            'usoOrderNo.5833520DE86E4C93A969A1E0C035000E.81938E76D2874C8881433422BA3FDEF0',
-                        },
-                        {
-                          title: '回单时效',
-                          dataIndex:
-                            'usoOrderNo.5833520DE86E4C93A969A1E0C035000E.E6B0BFA5CE63443EBFE1C832F7D5B804',
-                        },
-                        {
-                          title: '业务总时效',
-                          dataIndex:
-                            'usoOrderNo.5833520DE86E4C93A969A1E0C035000E.CC7C6AB9312C4A0199B7A0DD0E443E26',
-                        },
-                        {
-                          title: '履约时效',
-                          dataIndex:
-                            'usoOrderNo.5833520DE86E4C93A969A1E0C035000E.1E7765AB555946A0915E559587D85967',
-                        },
-                        {
-                          title: '物流总时效',
-                          dataIndex:
-                            'usoOrderNo.5833520DE86E4C93A969A1E0C035000E.62DDD6D6C3FB44B9B934F177D862C122',
-                        },
-                      ],
+                      title: 'C端货源占用时效',
+                      dataIndex:
+                        'B8F385C4E8DD404FB955564A0B48EF1A.933565A0E2D84F4B987B9D48B2C23A02',
                     },
-                    { title: '财务', dataIndex: 'usoOrderNo.ASD32WC4E8DD409FB923564A0B48EF1' },
-                    { title: '财务', dataIndex: 'usoOrderNo.157A6975A4414D5585BB617EB2F095E1' },
                   ],
                 },
+                {
+                  title: '物流',
+                  dataIndex: '5833520DE86E4C93A969A1E0C035000E',
+                  children: [
+                    {
+                      title: 'C端派单时效',
+                      dataIndex:
+                        '5833520DE86E4C93A969A1E0C035000E.069EB075765349F6923524C192A98E94',
+                    },
+                    {
+                      title: '确定物流方式时效',
+                      dataIndex:
+                        '5833520DE86E4C93A969A1E0C035000E.337CC46F72BA4D7F9D671D09811255E4',
+                    },
+                    {
+                      title: '归集时效',
+                      dataIndex:
+                        '5833520DE86E4C93A969A1E0C035000E.28F28765B1504FC38046F61E2B22E3AF',
+                    },
+                    {
+                      title: '配送时效',
+                      dataIndex:
+                        '5833520DE86E4C93A969A1E0C035000E.2CB097DC3D9D49348627EE67CD084FCD',
+                    },
+                    {
+                      title: '民生派单时效',
+                      dataIndex:
+                        '5833520DE86E4C93A969A1E0C035000E.7D418FDA8CAC49AEA6DAB4F1DFC3D824',
+                    },
+                    {
+                      title: 'C端出库时效',
+                      dataIndex:
+                        '5833520DE86E4C93A969A1E0C035000E.D0F7DF97761C49899487D315FA2AE1DA',
+                    },
+                    {
+                      title: '到车时效',
+                      dataIndex:
+                        '5833520DE86E4C93A969A1E0C035000E.592326EE61BB45C08787319422ED7082',
+                    },
+                    {
+                      title: '装车时效',
+                      dataIndex:
+                        '5833520DE86E4C93A969A1E0C035000E.39BAB84FFEBF411789EC1766F3C6F369',
+                    },
+                    {
+                      title: '在途时效',
+                      dataIndex:
+                        '5833520DE86E4C93A969A1E0C035000E.81938E76D2874C8881433422BA3FDEF0',
+                    },
+                    {
+                      title: '回单时效',
+                      dataIndex:
+                        '5833520DE86E4C93A969A1E0C035000E.E6B0BFA5CE63443EBFE1C832F7D5B804',
+                    },
+                    {
+                      title: '业务总时效',
+                      dataIndex:
+                        '5833520DE86E4C93A969A1E0C035000E.CC7C6AB9312C4A0199B7A0DD0E443E26',
+                    },
+                    {
+                      title: '履约时效',
+                      dataIndex:
+                        '5833520DE86E4C93A969A1E0C035000E.1E7765AB555946A0915E559587D85967',
+                    },
+                    {
+                      title: '物流总时效',
+                      dataIndex:
+                        '5833520DE86E4C93A969A1E0C035000E.62DDD6D6C3FB44B9B934F177D862C122',
+                    },
+                  ],
+                },
+                { title: '财务', dataIndex: 'ASD32WC4E8DD409FB923564A0B48EF1' },
+                { title: '财务', dataIndex: '157A6975A4414D5585BB617EB2F095E1' },
               ],
               searchCount: true,
               pages: 18596,
