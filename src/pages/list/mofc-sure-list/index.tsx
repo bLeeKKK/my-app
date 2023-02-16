74d8ad4cd893ce3a4764ae3bda02025a23afc95d
李槟 <1961774471@qq.com>
2023-02-16 16:49:37 +0800

    接口修改

--- src/pages/list/mofc-sure-list/index.tsx
+++ src/pages/list/mofc-sure-list/index.tsx
@@ -23,10 +23,9 @@ function getHaveTDate(arr: any[]) {
   ];
 }
 
-// mofc状态 初始,接单中,已接单,已出库,已揽件,部分签收,已拒签,已签收,取消中,已取消
-const STATUS_OPTIONS = {
+// eboc状态：初始,接单中,已接单,已出库,已揽件,部分签收,已拒签,已签收,取消中,已取消
+const EBOC_STATUS_OPTIONS = {
   ['初始']: { text: '初始', status: '初始' },
-  ['已派单']: { text: '已派单', status: '已派单' },
   ['接单中']: { text: '接单中', status: '接单中' },
   ['已接单']: { text: '已接单', status: '已接单' },
   ['已出库']: { text: '已出库', status: '已出库' },
@@ -36,6 +35,29 @@ const STATUS_OPTIONS = {
   ['已签收']: { text: '已签收', status: '已签收' },
   ['取消中']: { text: '取消中', status: '取消中' },
   ['已取消']: { text: '已取消', status: '已取消' },
+};
+// mofc状态：新建，已派单,已接单,已取消,已派车,已揽件,部分发货,已发货,部分起运,已起运,部分在途,已在途,部分拒签,已拒签,部分签收,已签收,部分回单,已回单,已撤回,已作废
+const MOFC_STATUS_OPTIONS = {
+  ['新建']: { text: '新建', status: '新建' },
+  ['已派单']: { text: '已派单', status: '已派单' },
+  ['已接单']: { text: '已接单', status: '已接单' },
+  ['已取消']: { text: '已取消', status: '已取消' },
+  ['已派车']: { text: '已派车', status: '已派车' },
+  ['已揽件']: { text: '已揽件', status: '已揽件' },
+  ['部分发货']: { text: '部分发货', status: '部分发货' },
+  ['已发货']: { text: '已发货', status: '已发货' },
+  ['部分起运']: { text: '部分起运', status: '部分起运' },
+  ['已起运']: { text: '已起运', status: '已起运' },
+  ['部分在途']: { text: '部分在途', status: '部分在途' },
+  ['已在途']: { text: '已在途', status: '已在途' },
+  ['部分拒签']: { text: '部分拒签', status: '部分拒签' },
+  ['已拒签']: { text: '已拒签', status: '已拒签' },
+  ['部分签收']: { text: '部分签收', status: '部分签收' },
+  ['已签收']: { text: '已签收', status: '已签收' },
+  ['部分回单']: { text: '部分回单', status: '部分回单' },
+  ['已回单']: { text: '已回单', status: '已回单' },
+  ['已撤回']: { text: '已撤回', status: '已撤回' },
+  ['已作废']: { text: '已作废', status: '已作废' },
 };
 
 const MofcSureList: React.FC = () => {
@@ -64,7 +86,13 @@ const MofcSureList: React.FC = () => {
     },
     { title: 'EBOC发货编号', dataIndex: 'ebocFhNo', width: 140 },
     { title: 'EBOC单据号', dataIndex: 'ebocLNo', width: 140 },
-    { title: 'EBOC状态', dataIndex: 'ebocStatus', width: 140 },
+    {
+      title: 'EBOC状态',
+      dataIndex: 'ebocStatus',
+      width: 140,
+      valueEnum: EBOC_STATUS_OPTIONS,
+      valueType: 'select',
+    },
     {
       title: 'MOFC删除标记',
       dataIndex: 'mofcDelFlag',
@@ -81,7 +109,7 @@ const MofcSureList: React.FC = () => {
       title: 'MOFC状态',
       dataIndex: 'serverStatus',
       width: 140,
-      valueEnum: STATUS_OPTIONS,
+      valueEnum: MOFC_STATUS_OPTIONS,
       valueType: 'select',
     },
   ];
@@ -95,7 +123,7 @@ const MofcSureList: React.FC = () => {
         search={{ labelWidth: 120 }}
         // toolBarRender={() => [<Edit key="eidt" />]}
         sticky
-        // scroll={{ x: 1500 }}
+        scroll={{ x: 1500 }}
         // pagination={{
         //   pageSize: 30,
         // }}
--- src/pages/list/mofc-sure-list/service.ts
+++ src/pages/list/mofc-sure-list/service.ts
@@ -1,9 +1,9 @@
 import { request } from 'umi';
-// import { SERVER_PATH } from '@/services/constants';
+import { SERVER_PATH, BIZLOG_REPORT_CORE } from '@/services/constants';
 
 // 分页查询
 export async function findByPage(data) {
-  return request(`/report-inf/deliverEbocLeftMofc/findAllByPage`, {
+  return request(`${SERVER_PATH}/${BIZLOG_REPORT_CORE}/deliverEbocLeftMofc/findAllByPage`, {
     method: 'post',
     data,
   });
