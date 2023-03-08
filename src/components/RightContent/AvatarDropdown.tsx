eeb3e5cac5fe581c059e54daa663af67b88d0d47
lilinyu <1377821887@qq.com>
2023-03-08 16:40:32 +0800

    fix: 退出登录代码更新

--- src/components/RightContent/AvatarDropdown.tsx
+++ src/components/RightContent/AvatarDropdown.tsx
@@ -17,7 +17,8 @@ export type GlobalHeaderRightProps = {
  * 退出登录，并且将当前的 url 保存
  */
 const loginOut = async () => {
-  await outLogin();
+  // await outLogin();
+  localStorage.clear();
   const { query = {}, search, pathname } = history.location;
   const { redirect } = query;
   // Note: There may be security issues, please note
