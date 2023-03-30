a1eaefd18fedbb6d114f2e884fc9aa607c5cb6bf
lilinyu <1377821887@qq.com>
2023-03-30 14:19:00 +0800

    全链路数据列表按条件渲染

--- src/pages/list/base-show-list-no/components/ShowBox/index.tsx
+++ src/pages/list/base-show-list-no/components/ShowBox/index.tsx
@@ -20,9 +20,9 @@ export default function ShowBox({ data }) {
   const sourceSysList = data?.sourceSysList;
   const [now, setNow] = useState(new Date());
 
-  useRafInterval(() => {
-    setNow(new Date());
-  }, 1000);
+  // useRafInterval(() => {
+  //   setNow(new Date());
+  // }, 1000);
 
   return (
     <div className={styles.warper}>
@@ -37,45 +37,75 @@ export default function ShowBox({ data }) {
                   return (
                     <div className={styles['small-nodes-title']} key={res.nodeName}>
                       <div>{res.nodeName}</div>
-                      {res?.fullLinkRecordVoList?.map((res) => (
-                        <div className={styles['small-node-show']} key={res.smallNodeCode}>
-                          <div className={styles['show-left']}>
-                            <div className={styles['show-left-name']}>【{res.smallNodeName}】</div>
-                            <div>待办：{res.agendaCause || '-'}</div>
-                            <div>
-                              <Tag color={res.signColor}>{res.overTimeRemark}</Tag>
+                      {res?.fullLinkRecordVoList?.map((res) => {
+
+                        //根据节点名字控制渲染方式
+                        console.log(res);
+                        if (res.nodeCode === 'MOFC_Order_B105' ||
+                          res.nodeCode === 'MOFC_Order_B106' ||
+                          res.nodeCode === 'LRP_Dispatch_B162' ||
+                          res.nodeCode === 'LRP_Dispatch_B172') {
+                          return res.remark
+                        } else if (res.nodeCode === 'product_source_type') {
+                          if (res.sourceType === 0) {
+                            return '现货';
+                          } else if (res.sourceType === 1) {
+                            return '期货';
+                          } else if (res.sourceType === 2) {
+                            return 'C+3';
+                          } else {
+                            return '-';
+                          }
+                        } else if (res.nodeCode === 'MOFC_Order_B100' ||
+                          res.nodeCode === 'MOFC_Order_B101' ||
+                          res.nodeCode === 'MOFC_Order_B104' ||
+                          res.nodeCode === 'LRP_Dispatch_B112' ||
+                          res.nodeCode === 'LRP_Dispatch_B122') {
+                          return res.remark ? moment(res.remark).format('YYYY-MM-DD HH:mm:ss') : '-';
+                        }
+                        else {
+                          return (
+                            <div className={styles['small-node-show']} key={res.smallNodeCode}>
+                              <div className={styles['show-left']}>
+                                <div className={styles['show-left-name']}>【{res.smallNodeName}】</div>
+                                <div>待办：{res.agendaCause || '-'}</div>
+                                <div>
+                                  <Tag color={res.signColor}>{res.overTimeRemark}</Tag>
+                                </div>
+                              </div>
+                              <div className={styles['show-right']}>
+                                <div>
+                                  开始时间：{moment(res.startDate).format('YYYY-MM-DD HH:mm:ss')}
+                                </div>
+                                <div>
+                                  结束时间：
+                                  {res.endDate
+                                    ? moment(res.endDate || now).format('YYYY-MM-DD HH:mm:ss')
+                                    : '处理中'}
+                                </div>
+                                {/* <Popover
+          content={
+            <>
+              <div>
+                开始时间：{moment(res.startDate).format('YYYY-MM-DD HH:mm:ss')}
+              </div>
+              <div>
+                结束时间：
+                {res.endDate
+                  ? moment(res.endDate).format('YYYY-MM-DD HH:mm:ss')
+                  : '处理中'}
+              </div>
+            </>
+          }
+        > */}
+                                <div>{timeDiff(res.startDate, res.endDate || now)}</div>
+                                {/* </Popover> */}
+                              </div>
                             </div>
-                          </div>
-                          <div className={styles['show-right']}>
-                            <div>
-                              开始时间：{moment(res.startDate).format('YYYY-MM-DD HH:mm:ss')}
-                            </div>
-                            <div>
-                              结束时间：
-                              {res.endDate
-                                ? moment(res.endDate || now).format('YYYY-MM-DD HH:mm:ss')
-                                : '处理中'}
-                            </div>
-                            {/* <Popover
-                              content={
-                                <>
-                                  <div>
-                                    开始时间：{moment(res.startDate).format('YYYY-MM-DD HH:mm:ss')}
-                                  </div>
-                                  <div>
-                                    结束时间：
-                                    {res.endDate
-                                      ? moment(res.endDate).format('YYYY-MM-DD HH:mm:ss')
-                                      : '处理中'}
-                                  </div>
-                                </>
-                              }
-                            > */}
-                            <div>{timeDiff(res.startDate, res.endDate || now)}</div>
-                            {/* </Popover> */}
-                          </div>
-                        </div>
-                      ))}
+                          )
+                        }
+
+                      })}
                     </div>
                   );
                 })
