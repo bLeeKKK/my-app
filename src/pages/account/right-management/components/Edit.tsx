c07b5070ae2f488453b7fabab26daf62e5001b6d
李槟 <1961774471@qq.com>
2023-05-15 10:34:09 +0800

    完善权限管理配置

--- src/pages/account/right-management/components/Edit.tsx
+++ src/pages/account/right-management/components/Edit.tsx
@@ -1,5 +1,12 @@
 import { message, Form, Button } from 'antd';
-import { ModalForm, ProFormText, ProForm, ProFormTreeSelect } from '@ant-design/pro-form';
+import {
+  ProFormRadio,
+  ModalForm,
+  ProFormText,
+  ProForm,
+  ProFormTreeSelect,
+  ProFormDigit,
+} from '@ant-design/pro-form';
 import { save, treeselect } from '../service';
 import type { ParamsType } from '../service';
 import { PlusOutlined } from '@ant-design/icons';
@@ -8,8 +15,8 @@ import { useUpdateEffect } from 'ahooks';
 import { getModel } from '@/utils';
 
 export const STATUS_OPTIONS = [
-  { value: 0, label: '正常' },
-  { value: 1, label: '停用' },
+  { value: true, label: '正常' },
+  { value: false, label: '停用' },
 ];
 
 const handleAdd = async (data: ParamsType) => {
@@ -85,10 +92,7 @@ export default function AddModalForm() {
         title={`${getModel(editType)}权限`}
         width="800px"
         visible={visible}
-        // formRef={formRef}
         form={form}
-        // labelCol={{ span: 10 }}
-        // wrapperCol={{ span: 14 }}
         onVisibleChange={(flag) => {
           if (!flag) closeModal();
         }}
@@ -111,6 +115,7 @@ export default function AddModalForm() {
             name="parentId"
             label="上级权限"
             width="sm"
+            initialValue={0}
             fieldProps={{
               fieldNames: {
                 value: 'id',
@@ -120,9 +125,34 @@ export default function AddModalForm() {
             }}
             request={async () => {
               const { data = [] } = await treeselect();
-
-              return data;
+              const warrperData = [
+                {
+                  children: data,
+                  id: 0,
+                  label: '主目录',
+                },
+              ];
+              return warrperData;
             }}
+          />
+          <ProFormRadio.Group
+            name="menuType"
+            initialValue="M"
+            label="菜单类型"
+            options={[
+              {
+                label: '目录',
+                value: 'M',
+              },
+              {
+                label: '菜单',
+                value: 'C',
+              },
+              {
+                label: '功能项',
+                value: 'F',
+              },
+            ]}
           />
         </ProForm.Group>
         <ProForm.Group>
@@ -139,10 +169,65 @@ export default function AddModalForm() {
             rules={[{ required: true, message: '必须权限标识' }]}
           />
           <ProFormText
+            tooltip="访问的路由地址，如：`user`，如外网地址需内链访问则以`http(s)://`开头"
             name="path"
             label="路由地址"
             width="sm"
             rules={[{ required: true, message: '必须路由地址' }]}
+          />
+        </ProForm.Group>
+        <ProForm.Group>
+          <ProFormDigit
+            // rules={[{ required: true, message: '必须排序' }]}
+            label="显示排序"
+            name="orderNum"
+            width="sm"
+            min={1}
+          />
+        </ProForm.Group>
+        <ProForm.Group>
+          <ProFormRadio.Group
+            tooltip="选择是外链则路由地址需要以`http(s)://`开头"
+            name="isFrame"
+            initialValue={false}
+            label="是否外链"
+            width="sm"
+            options={[
+              {
+                label: '是',
+                value: true,
+              },
+              {
+                label: '否',
+                value: false,
+              },
+            ]}
+          />
+
+          <ProFormRadio.Group
+            tooltip="选择隐藏则路由将不会出现在侧边栏，但仍然可以访问"
+            name="visible"
+            initialValue={true}
+            label="显示状态"
+            width="sm"
+            options={[
+              {
+                label: '显示',
+                value: true,
+              },
+              {
+                label: '隐藏',
+                value: false,
+              },
+            ]}
+          />
+          <ProFormRadio.Group
+            tooltip="选择停用则路由将不会出现在侧边栏，也不能被访问"
+            name="status"
+            initialValue={true}
+            label="菜单状态"
+            width="sm"
+            options={STATUS_OPTIONS}
           />
         </ProForm.Group>
       </ModalForm>
