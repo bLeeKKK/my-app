55717f2ace82f3fd6e456c895702bc42705189de
bin6.li <bin6.li@changhong.com>
2023-11-28 10:54:34 +0800

    优化展示分页

--- src/pages/dfc-reconciliation/show-list/service.ts
+++ src/pages/dfc-reconciliation/show-list/service.ts
@@ -1,11 +1,39 @@
 import { request } from 'umi';
 import { BIZLOG_CORE, SERVER_PATH } from '@/services/constants';
 
-export async function findByPage(data: any): Promise<{ data: any }> {
+export const getSearchData = (data: any) => {
+  // const asc = [];
+  // const desc = [];
+  // if (sort.eventStDatetime === 'descend') desc.push('event_st_datetime');
+  // if (sort.eventEndDatetime === 'descend') desc.push('event_end_datetime');
+  // if (sort.intfStDatetime === 'descend') desc.push('intf_st_datetime');
+  // if (sort.intfEndDatetime === 'descend') desc.push('intf_end_datetime');
+  // if (sort.eventFinishInterval === 'descend') desc.push('event_finish_interval');
+  // if (sort.receiveDataInterval === 'descend') desc.push('receive_data_interval');
+  //
+  // if (sort.eventStDatetime === 'ascend') asc.push('event_st_datetime');
+  // if (sort.eventEndDatetime === 'ascend') asc.push('event_end_datetime');
+  // if (sort.intfStDatetime === 'ascend') asc.push('intf_st_datetime');
+  // if (sort.intfEndDatetime === 'ascend') asc.push('intf_end_datetime');
+  // if (sort.eventFinishInterval === 'ascend') asc.push('event_finish_interval');
+  // if (sort.receiveDataInterval === 'ascend') asc.push('receive_data_interval');
+
+  return {
+    data,
+    params: {
+      size: data.pageSize,
+      current: data.current,
+      // desc: desc.join(','),
+      // asc: asc.join(','),
+    },
+  };
+};
+
+export async function findByPage(params: any): Promise<{ data: any }> {
+  const data = getSearchData(params);
   return request(`${SERVER_PATH}/${BIZLOG_CORE}/dfcdzResult/findByPage`, {
     method: 'POST',
-    data,
-    params: data,
+    ...data,
   });
 }
 
