d57b3bf041629ccffe4b3f202440ebfb86ceb18c
罗桢竺 <lzzhu1997@qq.com>
2024-11-21 10:10:14 +0800

    修改代理

--- config/config.ts
+++ config/config.ts
@@ -97,7 +97,7 @@ export default defineConfig({
           entry:
             process.env.NODE_ENV === 'development'
               ? 'http://localhost:8001'
-              : 'https://decmp.changhong.com/bizlog-web/index.html',
+              : 'https://ecmp.changhong.com/bizlog-web/index.html',
         },
         // {
         //   name: 'dfc-reconciliation',
